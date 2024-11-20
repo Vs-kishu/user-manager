cf1b96a24c74ab3a728abf52ebc7055ae165c21e
Krishna Kant Verma <kishanverma1208143@gmail.com>
2024-11-20 22:28:11 +0530

    increased expiration time for the jwt

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -22,7 +22,7 @@ exports.login = async (req, res) => {
     if (!user || user.isBlocked) return res.status(400).send("Invalid credentials or blocked user");
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).send("Invalid credentials");
-    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "10d" });
     res.json({ token,user });
   } catch (error) {
     res.status(500).send("Server error");
